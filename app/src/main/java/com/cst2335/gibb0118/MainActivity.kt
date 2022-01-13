28f86059f5022c7f509d2ba8c8026bab1c24d5f7
justmedusty <dustyn_gibb@hotmail.com>
2022-01-13 13:57:47 -0500

    Initial commit

---
+++ app/src/main/java/com/cst2335/gibb0118/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cst2335.gibb0118
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
