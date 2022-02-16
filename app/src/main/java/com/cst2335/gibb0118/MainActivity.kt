2a0d3a83ecee6f778c008fb4a47c9291d912adeb
justmedusty <dustyn_gibb@hotmail.com>
2022-02-16 15:22:57 -0500

    Finished Lab3

--- app/src/main/java/com/cst2335/gibb0118/MainActivity.kt
+++ app/src/main/java/com/cst2335/gibb0118/MainActivity.kt
@@ -1,41 +1,89 @@
 package com.cst2335.gibb0118
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
-import androidx.core.view.accessibility.AccessibilityEventCompat.setAction
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_relative)
+        setContentView(R.layout.lab3)
 
-        val button = findViewById<Button>(R.id.button2)
-        val switch = findViewById<Switch>(R.id.switch3)
+        loadCredentials()
+        checkEmptyFields()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        saveCredentials()
+    }
+
+
+    private fun loadCredentials() {
+        val email = findViewById<EditText>(R.id.editTextEmailAddress)
+        val password = findViewById<EditText>(R.id.editTextPassword)
+
+        val sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+        val savedEmail: String = sharedPreferences.getString("EMAIL", "").toString()
+        val savedPassword: String = sharedPreferences.getString("PASSWORD", "").toString()
+
+        email.setText(savedEmail)
+        password.setText(savedPassword)
+
+    }
+
+
+    private fun saveCredentials() {
+
+        val email = findViewById<EditText>(R.id.editTextEmailAddress)
+        val password = findViewById<EditText>(R.id.editTextPassword)
+        val insertedEmail = email.text.toString()
+        val insertedPassword = password.text.toString()
+
+        val sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+
+        val edit: SharedPreferences.Editor = sharedPreferences.edit()
+
+        edit.apply {
+            putString("EMAIL", insertedEmail)
+            putString("PASSWORD", insertedPassword)
+        }.apply()
+        Toast.makeText(this, "Credentials saved", Toast.LENGTH_LONG).show()
+    }
+
+
+    private fun checkEmptyFields() {
+
+        val button = findViewById<Button>(R.id.button5)
+        val email = findViewById<EditText>(R.id.editTextEmailAddress)
+        val password = findViewById<EditText>(R.id.editTextPassword)
 
         button.setOnClickListener {
-            Toast.makeText(
-                applicationContext,
-                resources.getString(R.string.moreInfoToast),
-                Toast.LENGTH_LONG
-            ).show()
+            if (email.text.isNullOrBlank() || password.text.isNullOrBlank()) {
+                Toast.makeText(this, "Please fill in the fields", Toast.LENGTH_SHORT).show()
+            } else nextActivity()
         }
+    }
 
-        switch.setOnCheckedChangeListener { compoundButton, b ->
-            if (b){
-                Snackbar.make(compoundButton,resources.getString(R.string.SnackbarOn),Snackbar.LENGTH_LONG).show()
-            }
-            else if (!b){
-                Snackbar.make(compoundButton,resources.getString(R.string.SnackBarOff),Snackbar.LENGTH_LONG).show()
-            }
-        }
+    private fun nextActivity() {
 
+        val nextPage = Intent(this, ProfileActivity::class.java)
+        nextPage.putExtra("EMAIL","EMAIL")
+
+        startActivity(nextPage)
 
     }
 
 
 }
+
+
 
 
---
+++ app/src/main/java/com/cst2335/gibb0118/ProfileActivity.kt
@@ -0,0 +1,92 @@
+package com.cst2335.gibb0118
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.*
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val imageButton = findViewById<ImageButton>(R.id.imageButton4)
+        updateEmail()
+
+        imageButton.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "Paused")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(TAG, "Restart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "Resumed")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "Destroyed")
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+
+            myPictureTakerLauncher.launch(takePictureIntent)
+        }
+
+
+    }
+
+    private val myPictureTakerLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        val imageButton = findViewById<ImageButton>(R.id.imageButton4)
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                val data: Intent = result.data!!
+                val imgBitMap: Bitmap = data.extras?.get("data") as Bitmap
+                imageButton.setImageBitmap(imgBitMap)
+            }
+            Activity.RESULT_CANCELED -> {
+                Log.i(TAG, "User refused to capture image")
+            }
+        }
+    }
+
+
+    private fun updateEmail() {
+        val sharedPreferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+        val email = findViewById<EditText>(R.id.editTextTextPersonName5)
+        val fromMain: Intent = intent
+        fromMain.getStringExtra("EMAIL")
+        email.setText(sharedPreferences.getString("EMAIL", "EMAIL"))
+    }
+}
